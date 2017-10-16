8cbda3ad2e9289684c966888e970f083a957e62f
YanChan  Lin <yonalin1@gmail.com>
2017-10-16 16:09:21 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   return "I LOVE YOU TOO PUMPKIN!"
 elsif phrase == phrase.upcase
   return "NO, NOT SINCE 1938!"
-else  
+else
   return "HUH?! SPEAK UP, SONNY!"
 
   end
