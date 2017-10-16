9eb1add7cd571e219a9a23c391c0ac62d120e3b7
YanChan  Lin <yonalin1@gmail.com>
2017-10-16 16:04:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
   # if phrase == phrase.downcase
-  puts "HUH?! SPEAK UP, SONNY!"
+  return "HUH?! SPEAK UP, SONNY!"
 # elsif phrase == phrase.upcase
-#   puts "NO, NOT SINCE 1938!"
+#   return "NO, NOT SINCE 1938!"
 # else phrase == "I LOVE YOU GRANDMA!"
-#     puts "I LOVE YOU TOO PUMPKIN!"
+#     return "I LOVE YOU TOO PUMPKIN!"
   # end
  end
 
